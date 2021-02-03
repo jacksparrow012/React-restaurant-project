4f9c21f80fdfdb027d9b51e77b75ef8b0527b0ea
Nazrul islam <jacksparrow.alex@gmail.com>
2021-02-03 22:41:36 +0600

    form is ready for getting input field vlaue by react controlled form system

--- src/components/body/Contact.js
+++ src/components/body/Contact.js
@@ -1,52 +1,70 @@
 import React, { Component } from 'react';
 import { Form, FormGroup, Input, Label, Button } from "reactstrap"
 class Contact extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstname: "",
+            lastname: "",
+            telnum: "",
+            email: "",
+            agree: true,
+            message: "",
+            contactType: "Tel."
+        }
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleInputChange = event => {
+        const value = event.target.type === "checkbox" ? event.target.checked : event.target.value
+        const name = event.target.name
+        this.setState({
+            [name]: value
+        })
+    }
+    handleSubmit = event => {
+        console.log(this.state);
+        event.preventDefault()
+    }
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col">
                         <h2>Send Your Feedback to Us</h2>
-                        <Form className="myForm">
+                        <Form className="myForm" onSubmit={this.handleSubmit}>
                             <Label></Label>
                             <FormGroup>
                                 <Input type="text"
                                     name="firstname"
                                     placeholder="Your First Name"
+                                    value={this.state.firstname}
+                                    onChange={this.handleInputChange}
                                 />
                             </FormGroup>
                             <FormGroup>
                                 <Input type="text"
                                     name="lastname"
                                     placeholder="Your Last Name"
+                                    value={this.state.lastname}
+                                    onChange={this.handleInputChange}
                                 />
                             </FormGroup>
                             <FormGroup>
                                 <Input type="number"
                                     name="telnum"
                                     placeholder="Your Contact Number"
+                                    value={this.state.telnum}
+                                    onChange={this.handleInputChange}
                                 />
                             </FormGroup>
                             <FormGroup>
                                 <Input type="email"
                                     name="email"
                                     placeholder="Your Email"
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange}
                                 />
-                            </FormGroup>
-
-                            <FormGroup>
-                                <div className="row">
-                                    <div className="col-6">
-                                        <h4>How we will contact with you?</h4>
-                                    </div>
-
-                                    <div className="col-6">
-                                        <Input type="select" name="contactType">
-                                            <option>Tel.</option>
-                                            <option>Email</option>
-                                        </Input>
-                                    </div>
-                                </div>
                             </FormGroup>
                             <FormGroup>
                                 <div className="row">
@@ -54,12 +72,37 @@ class Contact extends Component {
                                         <h5>Can we contact with you?</h5>
                                     </div>
                                     <div className="col-6">
-                                        <Input type="checkbox" name="agree" />
+                                        <Input type="checkbox" name="agree"
+                                            checked={this.state.agree}
+                                            onChange={this.handleInputChange}
+                                        />
                                     </div>
                                 </div>
                             </FormGroup>
                             <FormGroup>
-                                <Input type="textarea" placeholder="write something about our service"></Input>
+                                <div className="row">
+                                    <div className="col-6">
+                                        <h4>How we will contact with you?</h4>
+                                    </div>
+
+                                    <div className="col-6">
+                                        <Input type="select" name="contactType"
+                                            onChange={this.handleInputChange}>
+                                            <option>Tel.</option>
+                                            <option>Email</option>
+
+                                        </Input>
+                                    </div>
+                                </div>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Input type="textarea"
+                                    placeholder="write something about our service"
+                                    name="message"
+                                    value={this.state.message}
+                                    onChange={this.handleInputChange}
+                                ></Input>
                             </FormGroup>
 
                             <FormGroup>
